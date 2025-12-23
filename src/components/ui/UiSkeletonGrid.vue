f985a758a6065bbf5565c359babb6c2e967daddc
Fhaladin-Dev <farhan.nif.alaudin@gmail.com>
2025-12-23 18:35:44 +0700

    Layout: Update column sizes in UiSkeletonGrid for improved responsiveness based on view mode

--- src/components/ui/UiSkeletonGrid.vue
+++ src/components/ui/UiSkeletonGrid.vue
@@ -3,10 +3,10 @@
     <v-col
       v-for="i in 4"
       :key="i"
-      cols="12"
+      :cols="viewMode === 'grid' ? 6 : 12"
       height="200px"
-      :lg="viewMode === 'grid' ? 4 : 12"
-      :md="viewMode === 'grid' ? 6 : 12"
+      :lg="viewMode === 'grid' ? 3 : 12"
+      :md="viewMode === 'grid' ? 4 : 12"
     >
       <v-skeleton-loader
         class="mx-auto border"
